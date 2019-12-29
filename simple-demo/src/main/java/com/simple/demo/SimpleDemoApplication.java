ce336e9469a99f040047aa33702c1cf2351ee4cd
objectboy@163.com <objectboy@163.com>
2019-12-29 23:53:05 +0800

    添加配置

--- simple-demo/src/main/java/com/simple/demo/SimpleDemoApplication.java
+++ simple-demo/src/main/java/com/simple/demo/SimpleDemoApplication.java
@@ -3,8 +3,9 @@ package com.simple.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 
-@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class, DataSourceTransactionManagerAutoConfiguration.class })
 public class SimpleDemoApplication {
 
     public static void main(String[] args) {
